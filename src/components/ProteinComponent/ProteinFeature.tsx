e6adf6c9b6474819fc7acbf84f6e3fad9f330b4c
giorgi kikadze <giorgi.kikadze@quantori.com>
2023-05-24 15:17:53 +0400

    feat: create protein page, add details page

---
+++ src/components/ProteinComponent/ProteinFeature.tsx
@@ -0,0 +1,5 @@
+const ProteinFeature: React.FC = () => {
+  return <div className="protein flex">feature</div>;
+};
+
+export default ProteinFeature;
---
+++ src/components/ProteinComponent/ProteinPublications.tsx
@@ -0,0 +1,5 @@
+const ProteinPublications: React.FC = () => {
+  return <div className="protein flex">publication</div>;
+};
+
+export default ProteinPublications;
